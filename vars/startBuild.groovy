5b8991e4d8bb75f80b5061ddfc31f868ab1b1611
victor <victor@sevenos.akira.local>
2017-05-16 18:11:59 +0200

    adding first function

--- vars/startBuild.groovy
+++ vars/startBuild.groovy
@@ -4,8 +4,6 @@ def call(cluster, project, app){
     sh "rm -rf oc-build && mkdir -p oc-build/deployments"
     sh "cp target/*.jar oc-build/deployments/"
     openshift.withCluster(cluster){
-        openshift.withProject(project){
-            sh "oc start-build ${app}-container --from-dir=oc-build --follow"
-        }
+      sh "oc start-build ${app}-container --from-dir=oc-build --follow -n ${project}"
     }
 }
