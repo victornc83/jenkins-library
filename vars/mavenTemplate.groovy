2e9446f2548e39ba03accbb274a64e0453b342f4
victor <victor@sevenos.akira.local>
2017-05-17 10:19:23 +0200

    adding first function

---
+++ vars/mavenTemplate.groovy
@@ -0,0 +1,10 @@
+#!/usr/bin/groovy
+
+def call(namespace){
+    podTemplate(label: label,
+      namespace: namespace,
+      containers: [containerTemplate(name: 'maven', image: 'openshift/jenkins-slave-maven-centos7', args: '${computer.jnlpmac} ${computer.name}')]
+      ) {
+        body()
+      }
+}
--- vars/startBuild.groovy
+++ vars/startBuild.groovy
@@ -1,9 +1,7 @@
 #!/usr/bin/groovy
 
-def call(cluster, project, app){
+def call(app){
     sh "rm -rf oc-build && mkdir -p oc-build/deployments"
     sh "cp target/*.jar oc-build/deployments/"
-    openshift.withCluster(cluster){
-      sh "oc start-build ${app}-container --from-dir=oc-build --follow -n ${project}"
-    }
+    sh "oc start-build ${app}-container --from-dir=oc-build --follow"
 }
