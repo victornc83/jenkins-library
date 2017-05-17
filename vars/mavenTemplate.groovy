3c0a8ccac6033f65ae3307b395d6d49a636bdbf6
victor <victor@sevenos.akira.local>
2017-05-17 12:04:32 +0200

    testing

--- vars/mavenTemplate.groovy
+++ vars/mavenTemplate.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/groovy
 
 def call(namespace, body){
-    podTemplate(label: label,
+    podTemplate(label: 'maven-node',
       cloud: 'openshift',
       namespace: '${namespace}',
       containers: [containerTemplate(name: 'maven', image: 'openshift/jenkins-slave-maven-centos7', args: '${computer.jnlpmac} ${computer.name}')]
