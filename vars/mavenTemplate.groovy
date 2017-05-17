fe7a99025699ae96ae2a83f7f258f98c81c8f8a7
victor <victor@sevenos.akira.local>
2017-05-17 10:39:19 +0200

    adding podtemplate in groovy library

--- vars/mavenTemplate.groovy
+++ vars/mavenTemplate.groovy
@@ -1,8 +1,11 @@
 #!/usr/bin/groovy
 
-def call(namespace){
+def call(Map parameters = [:], body){
+
+    def namespace = parameters.get('namespace','myproject')
+
     podTemplate(label: label,
-      namespace: '${namespace}',
+      namespace: namespace,
       containers: [containerTemplate(name: 'maven', image: 'openshift/jenkins-slave-maven-centos7', args: '${computer.jnlpmac} ${computer.name}')]
       ) {
         body()
