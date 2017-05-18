74ae8930689f1d7e039bc12512b1fb687c723077
victor <victor@sevenos.akira.local>
2017-05-18 17:13:21 +0200

    testing

--- vars/waitDeployIsComplete.groovy
+++ vars/waitDeployIsComplete.groovy
@@ -4,7 +4,8 @@ def call(project, app){
   loginOpenshift(project){
     timeout(time: 2, unit: 'MINUTES') {
       waitUntil{
-        sh "oc get dc ${app} --template '{{range .status.conditions}}{{.status|println}}{{end}}' | while read line ; do if [ ${line} != "True" ] ; then return 1 ; fi ; done"
+        sh "oc get dc ${app} --template '{{range .status.conditions}}{{.status|println}}{{end}}' > status"
+        sh "cat status | while read line ; do if [ ${line} != "True" ] ; then return 1 ; fi ; done"
       }
     }
   }
