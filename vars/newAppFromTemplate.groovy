b6f262f261d9714ae7ea2fa786422b59a59b0811
victor <victor@sevenos.akira.local>
2017-05-18 19:11:58 +0200

    testing

--- vars/newAppFromTemplate.groovy
+++ vars/newAppFromTemplate.groovy
@@ -1,7 +1,16 @@
 #!/usr/bin/groovy
 
-def call(project, app){
-  loginOpenshift(project){
-    
+def call(body){
+
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+
+  def params = ''
+  loginOpenshift(config.project){
+    for(int i = 0; i<config.parameters.size();i++){
+      params = params + '-p ' + config.parameters[i] + '=' + config.values[i]
+    }
   }
 }
--- vars/waitDeployIsComplete.groovy
+++ vars/waitDeployIsComplete.groovy
@@ -2,7 +2,7 @@
 
 def call(project, app){
   loginOpenshift(project){
-    timeout(time: 2, unit: 'MINUTES') {
+    timeout(time: 5, unit: 'MINUTES') {
       waitUntil{
         sh "oc get rc myapp-\$(oc get dc ${app} --template={{.status.latestVersion}}) --template={{.status.readyReplicas}} > status"
         def output = sh script:'''
