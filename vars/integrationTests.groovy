195af5b1b40771cedc7da59b09eddace8e4011a7
victor <victor@sevenos.akira.local>
2017-05-18 14:07:34 +0200

    testing

--- vars/integrationTests.groovy
+++ vars/integrationTests.groovy
@@ -2,11 +2,11 @@
 
 def call(project, app){
   loginOpenshift(project){
-    sh """
+    sh '''
     export host=$(oc get svc ${app} --template='{{.spec.clusterIP}}'
     export port=$(oc get svc ${app} --template='{{range .spec.ports}}{{.port}}{{end}}'
     mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent-integration verify -Dsurefire.skip=true -Dservice.endpoint=http://${host}:${p.port}
-    """
+    '''
     try {
       step([$class: 'JUnitResultArchiver', testResults: '**/target/failsafe-reports/TEST-*.xml'])
     } catch (err) {
