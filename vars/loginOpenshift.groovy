5f8e0ccfbe3bb134aef5389a510c93c94a764123
victor <victor@sevenos.akira.local>
2017-05-18 13:15:51 +0200

    testing

--- vars/loginOpenshift.groovy
+++ vars/loginOpenshift.groovy
@@ -2,7 +2,7 @@
 
 def call(project, body){
   withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'openshift', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-      sh "oc login --insecure-skip-tls-verify=true -u $env.USERNAME -p $env.PASSWORD $OPENSHIFT"
+      sh "oc login --insecure-skip-tls-verify=true -u $env.USERNAME -p $env.PASSWORD https://kubernetes.default.svc:443"
   }
   sh "oc new-project ${project} || echo 'Project exists'"
   sh "oc project ${project}"
