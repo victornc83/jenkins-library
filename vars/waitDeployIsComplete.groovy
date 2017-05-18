3f49219d7fd841f2d9f981f204a553cc045529b4
victor <victor@sevenos.akira.local>
2017-05-18 16:43:22 +0200

    testing

--- vars/waitDeployIsComplete.groovy
+++ vars/waitDeployIsComplete.groovy
@@ -4,7 +4,7 @@ def call(project, app){
   loginOpenshift(project){
     timeout(time: 2, unit: 'MINUTES') {
       waitUntil{
-        sh "export status=$(oc get dc ${app} --template '{{range .status.conditions}}{{.status|println}}{{end}}')"
+        sh "export status=$(oc get dc '${app}' --template '{{range .status.conditions}}{{.status|println}}{{end}}')"
         sh '''
         for e in $status ; do
           echo ${e}
