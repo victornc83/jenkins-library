4779a3f27c18bb1bfe1ddb7e831e4821f2baa680
victor <victor@sevenos.akira.local>
2017-05-18 12:07:17 +0200

    testing

---
+++ vars/integrationTests.groovy
@@ -0,0 +1,13 @@
+#!/usr/bin/groovy
+
+def call(project, app){
+  loginOpenshift(project)
+  def host=["oc", "get", "svc", "${app}", "--template='{{.spec.clusterIP}}'" ].execute().text
+  def port=["oc", "get", "svc", "${app}", "--template='{{range .spec.ports}}{{.port}}{{end}}'" ].execute().text
+  sh "mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent-integration verify -Dsurefire.skip=true -Dservice.endpoint=http://${host}:${p.port}"
+  try {
+    step([$class: 'JUnitResultArchiver', testResults: '**/target/failsafe-reports/TEST-*.xml'])
+  } catch (err) {
+    echo "No integration test result were found: ${err}"
+  }
+}
