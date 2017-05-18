0fe2f75ff5661f109153bc1a8e192a293207a107
victor <victor@sevenos.akira.local>
2017-05-18 16:30:55 +0200

    testing

--- vars/waitDeployIsComplete.groovy
+++ vars/waitDeployIsComplete.groovy
@@ -4,12 +4,13 @@ def call(project, app){
   loginOpenshift(project){
     timeout(time: 2, unit: 'MINUTES') {
       waitUntil{
-        def output = sh script: '''
+        sh '''
+        #!/bin/bash
         for e in $(oc get dc ${app} --template '{{range .status.conditions}}{{.status|println}}{{end}}') ; do
           if [ "${e}" != "False" ] ; then return 1 ; fi
         done
         return 0
-        ''', returnStatus: true
+        '''
       }
     }
   }
