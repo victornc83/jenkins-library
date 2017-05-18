6b088efe2ace6c790a179d1251a9ae047881d513
victor <victor@sevenos.akira.local>
2017-05-18 17:49:01 +0200

    testing

--- vars/integrationTests.groovy
+++ vars/integrationTests.groovy
@@ -2,7 +2,7 @@
 
 def call(project, app){
   loginOpenshift(project){
-    waitDeployIsComplete('stage','app')
+    waitDeployIsComplete(project, app)
     sh '''
     export host=$(oc get svc ${app} --template='{{.spec.clusterIP}}')
     export port=$(oc get svc ${app} --template='{{range .spec.ports}}{{.port}}{{end}}')
