70b95e925d64c729b5dc03ed5383656f2750d8a0
victor <victor@sevenos.akira.local>
2017-05-18 18:57:12 +0200

    testing

--- vars/integrationTests.groovy
+++ vars/integrationTests.groovy
@@ -3,7 +3,7 @@
 def call(project, app){
   loginOpenshift(project){
     waitDeployIsComplete(project, app)
-    sh "export host=\$(oc get svc ${app} --template='{{.spec.clusterIP}})"
+    sh "export host=\$(oc get svc ${app} --template='{{.spec.clusterIP}}')"
     sh "export port=\$(oc get svc ${app} --template='{{range .spec.ports}}{{.port}}{{end}}')"
     sh "mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent-integration verify -Dsurefire.skip=true -Dservice.endpoint=http://${host}:${port}"
     try {
