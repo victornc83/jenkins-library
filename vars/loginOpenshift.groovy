4693c653a8b83ddccf1acbe6854b2973596338d9
victor <victor@sevenos.akira.local>
2017-05-18 12:58:21 +0200

    testing

--- vars/loginOpenshift.groovy
+++ vars/loginOpenshift.groovy
@@ -1,9 +1,10 @@
 #!/usr/bin/groovy
 
-def call(project){
+def call(project, body){
   withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'openshift', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
       sh "oc login --insecure-skip-tls-verify=true -u $env.USERNAME -p $env.PASSWORD $OPENSHIFT"
   }
   sh "oc new-project ${project} || echo 'Project exists'"
   sh "oc project ${project}"
+  body()
 }
--- vars/startBuild.groovy
+++ vars/startBuild.groovy
@@ -1,8 +1,9 @@
 #!/usr/bin/groovy
 
 def call(project, app){
-    loginOpenshift(project)
-    sh "rm -rf oc-build && mkdir -p oc-build/deployments"
-    sh "cp target/*.jar oc-build/deployments/"
-    sh "oc start-build ${app}-container --from-dir=oc-build --follow"
+    loginOpenshift(project){
+      sh "rm -rf oc-build && mkdir -p oc-build/deployments"
+      sh "cp target/*.jar oc-build/deployments/"
+      sh "oc start-build ${app}-container --from-dir=oc-build --follow"
+    }
 }
