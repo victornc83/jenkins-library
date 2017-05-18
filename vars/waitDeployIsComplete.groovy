4cc3923d02d3ff9f6593a23bf8e603f469fb1f28
victor <victor@sevenos.akira.local>
2017-05-18 17:15:55 +0200

    testing

--- vars/waitDeployIsComplete.groovy
+++ vars/waitDeployIsComplete.groovy
@@ -5,7 +5,7 @@ def call(project, app){
     timeout(time: 2, unit: 'MINUTES') {
       waitUntil{
         sh "oc get dc ${app} --template '{{range .status.conditions}}{{.status|println}}{{end}}' > status"
-        sh "cat status | while read line ; do if [ ${line} != "True" ] ; then return 1 ; fi ; done"
+        sh "for l in `cat status` ; do if [ ${l} != "True" ] ; then return 1 ; fi ; done"
       }
     }
   }
