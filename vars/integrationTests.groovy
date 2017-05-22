c86a0a1287a9aba80595480316222142befb2da9
victor <victor@sevenos.akira.local>
2017-05-22 17:18:41 +0200

    adding support for existing apps in destination project in promotion stage

--- vars/integrationTests.groovy
+++ vars/integrationTests.groovy
@@ -5,7 +5,7 @@ def call(project, app){
     waitDeployIsComplete(project, app)
     //def host = sh script: "",returnOutput: true
     //def port = sh script: "",returnOutput: true
-    sh "mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent-integration verify -Dsurefire.skip=true -Dservice.endpoint=http://\$(oc get svc ${app} --template='{{.spec.clusterIP}}'):\$(oc get svc ${app} --template='{{range .spec.ports}}{{.port}}{{end}}')"
+    sh "mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent-integration verify -Dsurefire.skip=true -Dservice.endpoint=http://\$(oc get svc ${app} --template='{{.spec.clusterIP}}' -n ${project}):\$(oc get svc ${app} --template='{{range .spec.ports}}{{.port}}{{end}}' -n ${project})"
     try {
       step([$class: 'JUnitResultArchiver', testResults: '**/target/failsafe-reports/TEST-*.xml'])
     } catch (err) {
