eaf3157ae9d34f99a48f9452d19b7f87927608eb
victor <victor@sevenos.akira.local>
2017-05-22 16:56:14 +0200

    adding support for existing apps in destination project in promotion stage

--- vars/mavenTemplate.groovy
+++ vars/mavenTemplate.groovy
@@ -3,7 +3,7 @@
 def call(project, body){
     podTemplate(name: 'maven',
       label: 'maven',
-      cloud: "openshift-${project}",
+      cloud: "openshift",
       namespace: project,
       containers: [containerTemplate(name: 'jnlp', image: 'openshift/jenkins-slave-maven-centos7', ttyEnabled: true, workingDir: '/tmp', args: '${computer.jnlpmac} ${computer.name}')]
       ) {
