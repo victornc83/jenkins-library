a1480ae665598ca17a3eaf4feb37e36e6a0eb2db
victor <victor@sevenos.akira.local>
2017-05-22 17:07:13 +0200

    adding support for existing apps in destination project in promotion stage

--- vars/waitDeployIsComplete.groovy
+++ vars/waitDeployIsComplete.groovy
@@ -4,7 +4,7 @@ def call(project, app){
   loginOpenshift(project){
     timeout(time: 5, unit: 'MINUTES') {
       waitUntil{
-        sh "oc get rc myapp-\$(oc get dc ${app} --template={{.status.latestVersion}}) --template={{.status.readyReplicas}} > status"
+        sh "oc get rc myapp-\$(oc get dc ${app} --template={{.status.latestVersion}} -n ${project}) --template={{.status.readyReplicas}} -n ${project} > status"
         def output = sh script:'''
         for line in `cat status` ; do [ ${line} == "1" ] ; done
         ''', returnStatus: true
