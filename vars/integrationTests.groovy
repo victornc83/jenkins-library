6ee44d5510c21248a69991b69c1328b836918291
victor <victor@sevenos.akira.local>
2017-05-22 17:36:07 +0200

    adding support for existing apps in destination project in promotion stage

--- vars/integrationTests.groovy
+++ vars/integrationTests.groovy
@@ -2,7 +2,6 @@
 
 def call(project, app){
   loginOpenshift(project){
-    waitDeployIsComplete(project, app)
     //def host = sh script: "",returnOutput: true
     //def port = sh script: "",returnOutput: true
     sh "mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent-integration verify -Dsurefire.skip=true -Dservice.endpoint=http://\$(oc get svc ${app} --template='{{.spec.clusterIP}}' -n ${project}):\$(oc get svc ${app} --template='{{range .spec.ports}}{{.port}}{{end}}' -n ${project})"
