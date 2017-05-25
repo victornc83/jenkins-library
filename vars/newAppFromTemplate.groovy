a8481a2cc3daab8355aaa99bd3479508f6663fee
victor <victor@sevenos.akira.local>
2017-05-25 17:20:42 +0200

    Adding route in Jenkinsfile for prod branch

--- vars/newAppFromTemplate.groovy
+++ vars/newAppFromTemplate.groovy
@@ -14,7 +14,9 @@ def call(body){
     }
     def exist = sh(script:"oc get dc ${config.name} -n ${config.project}",returnStatus: true)
     if (!exist){
+      sh "oc export secret/${config.name} -n ${config.project} -o yaml > secrets.yml"
       sh "oc process openshift//${config.template} ${params} | oc replace -n ${config.project} -f - 2>&1 | grep -v Service"
+      sh "oc replace -f secrets.yml -n ${config.project}"
     }else{
       sh "oc new-app --name=${config.name} --template=${config.template} ${params} -n ${config.project}"
     }
