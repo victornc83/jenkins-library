5089f39b031629ec409821acf4b367b3c19da353
victor <victor@sevenos.akira.local>
2017-05-31 15:58:22 +0200

    Adding nodejs template

--- vars/startBuild.groovy
+++ vars/startBuild.groovy
@@ -2,8 +2,6 @@
 
 def call(project, app){
     loginOpenshift(project){
-      sh "rm -rf oc-build && mkdir -p oc-build/deployments"
-      sh "cp target/*.jar oc-build/deployments/"
       sh "oc start-build ${app}-container --from-dir=oc-build --follow -n ${project}"
     }
 }
